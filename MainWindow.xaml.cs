f1c34a3e5c4a52b5f3412a041f7b4d40497d03c8
Aymeric Bizouarn <aymeric.bizouarn@hotmail.fr>
2024-06-19 14:18:53 +0200

    Add code

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Management;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace DefaultPrinter;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        string defaultPrinter = "";
+        List<string> _printers = new();
+        var printerQuery = new ManagementObjectSearcher("SELECT * from Win32_Printer");
+
+        foreach (var printer in printerQuery.Get())
+        {
+            var name = (string) printer.GetPropertyValue("Name");
+            if ((bool) printer.GetPropertyValue("Default"))
+                defaultPrinter = name;
+            _printers.Add(name);
+        }
+
+        Printers.ItemsSource = _printers;
+        Printers.SelectedValue = defaultPrinter;
+        Valider.Click += ValidateOnClick;
+
+        void ValidateOnClick(object sender, RoutedEventArgs e)
+        {
+            SetDefaultPrinter(Printers.SelectedValue.ToString());
+        }
+
+        [DllImport("winspool.drv", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool SetDefaultPrinter(string Printer);
+    }
+}
